f27bdee84fbe063cdd0f1895cb2e66100a520242
Andrey bp <andreyktn@gmail.com>
2023-10-21 19:09:44 +0300

    editing

--- src/backend/tracker/models.py
+++ src/backend/tracker/models.py
@@ -52,53 +52,55 @@ class Vacancy(models.Model):
 
 
 MIN_WEIGHT_SKILL, MAX_WEIGHT_SKILL = 1, 5
-class VacansySkill(models.Model):
 
+class VacancySkill(models.Model):
+    """Промежуточная Модель Вакансии и скиллов."""
     vacancy = models.ForeignKey(
-        verbose_name='Вакансия',
-        help_text='Вакансия',
+        verbose_name="Вакансии",
+        help_text='В каких вакансиях',
         related_name="skill",
         to=Vacancy,
         on_delete=models.CASCADE,
     )
     skill = models.ForeignKey(
-        verbose_name='Навык',
-        help_text='Навык',
+        verbose_name="Навыки",
+        help_text='Связанный навык',
         related_name="vacancy",
-        to=vacancy,
+        to=Skill,
         on_delete=models.CASCADE,
     )
-    weigth = models.PositiveSmallIntegerField(
-        verbose_name='Важность',
-        help_text='Важность навык',
+    weight = models.PositiveSmallIntegerField(
+        verbose_name="Вес",
+        help_text='Важность навыка',
         default=MIN_WEIGHT_SKILL,
         validators=(
             MinValueValidator(
                 MIN_WEIGHT_SKILL,
-                'Должен быть выше, чем {MIN_WEIGHT_SKILL}',
+                f'Как минимум {MIN_WEIGHT_SKILL}',
             ),
             MaxValueValidator(
                 MAX_WEIGHT_SKILL,
-                'Должен быть не меньше, чем {MAX_WEIGHT_SKILL}',
+                f'Как максимум {MAX_WEIGHT_SKILL}',
             ),
         ),
     )
 
     class Meta:
-        verbose_name = 'Навык'
-        verbose_name_plural = 'Навыки'
+        verbose_name = "Навык"
+        verbose_name_plural = "Навыки"
         constraints = (
             models.UniqueConstraint(
                 fields=(
                     "vacancy",
                     "skill",
                 ),
-                name='unique_vacancy_skill',
+                name="unique_vacancy_skill",
             ),
         )
 
-    def __str__(self) -> str:
-        return f"{self.vacancy} {self.skill} - {self.weigth}"
+    def __str__(self):
+        return f'{self.skill}({self.vacancy}) - {self.weight}'
+
 
 class Resume(models.Model):
     """Модель резюме."""
