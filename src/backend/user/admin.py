32bc2a177cfdb1a4e48f98e8c557be87f6fa2046
Andrey bp <andreyktn@gmail.com>
2023-10-22 09:15:37 +0300

    added models

--- src/backend/user/admin.py
+++ src/backend/user/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from user.models import User
+from user.models import User, Favorite
 
 
 @admin.register(User)
@@ -15,3 +15,8 @@ class UserAdmin(admin.ModelAdmin):
     search_fields = ("username", "email")
     list_filter = ("username", "email")
     empty_value_display = "--пусто--"
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'clicker', 'liked')
