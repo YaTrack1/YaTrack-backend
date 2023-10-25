6be640ea3483808096fa5021681c34e057670295
AndreyZyuzin <116785183+AndreyZyuzin@users.noreply.github.com>
2023-10-25 20:31:17 +0300

    Добывлены Skill в админку (#26)
    Co-authored-by: Andrey bp <andreyktn@gmail.com>

--- src/backend/resume/admin.py
+++ src/backend/resume/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 
-from resume.models import Resume
+from resume.models import Resume, SkillInResume
 
 # admin.site.register(City)
 # admin.site.register(ResumeSkill)
 
 
+class SkillInResumeInline(admin.StackedInline):
+    model = SkillInResume
+    extra = 0
+
+
 @admin.register(Resume)
 class ResumeAdmin(admin.ModelAdmin):
     list_display = ("title",)
+    inlines = (SkillInResumeInline,)
